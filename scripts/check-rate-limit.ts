a669d773da59ed50661dadb603953559594676b0
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-10-27 19:35:27 +0800

    chore: add comments for check-rate-limit script

--- scripts/check-rate-limit.ts
+++ scripts/check-rate-limit.ts
@@ -1,6 +1,8 @@
 const test = fetch("https://openrouter.ai/api/v1/chat/completions", {
     method: "POST",
     headers: {
+        // obviously this API key has been rotated.
+        // i forgot about this before public-ing the repo lol
         Authorization: "Bearer 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",
         "Content-Type": "application/json"
     },
